eb50e05728af112d02ce83b0c02720ad42a17eb0
zhaoyangwu <zhaoyang1018@hotmail.com>
2023-12-31 13:01:23 -0500

    feat: add rollup config

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,66 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ElementType,
+	ReactElementType
+} from 'shared/ReactTypes';
+
+// React Element
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'Custom'
+	};
+
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...children: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	const childrenLength = children.length;
+	if (childrenLength) {
+		if (childrenLength === 1) {
+			props.children = children[0];
+		} else {
+			props.children = children;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDev = jsx;
